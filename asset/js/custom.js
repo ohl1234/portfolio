50f36366dc1374b95b0b34e46a4aa7b3247077ae
ohhyerim <tmvkfmxk00@naver.com>
2022-09-19 19:17:30 +0900

    js 1차

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,26 +1,95 @@
 $(function(){
-    // 마우스커서 모바일땐 없애기
-    $(window).mousemove(function(e){
-        e.preventDefault();
-        gsap.to('.cursor',{
-            x:e.clientX,
-            y:e.clientY,
-            duration:.2,
-        })
+    //moust event
+    $(window).resize(function(){ 
+        if (window.innerWidth > 767) {  
+            $(window).mousemove(function(e){
+                e.preventDefault();
+                gsap.to('.cursor',{
+                    x:e.clientX,
+                    y:e.clientY,
+                    duration:.2,
+                })
+            })
+        
+            $('[data-hover]').on('mouseover',function(){
+                $('.cursor').addClass('cursor-over')
+                $(this).css('cursor','none');
+            })
+            $('[data-hover]').on('mouseleave',function(){
+                $('.cursor').removeClass('cursor-over')
+            })
+        }
+        
+    }).resize(); 
+
+    // Trigger event
+    gsap.set('.sc-visual .txt-line1',{xPercent:-180})
+    gsap.set('.sc-visual .txt-line2',{xPercent:180})
+    const visualAni = gsap.timeline({
+            scrollTrigger:{
+            trigger:'.sc-visual .sc-title',
+            start:'top bottom',
+            end:'bottom top',
+            scrub:1
+        }
+    });
+    visualAni.addLabel('a')
+    .to('.sc-visual .txt-line1',{xPercent:100},'a')
+    .to('.sc-visual .txt-line2',{xPercent:-100},'a')
+
+    gsap.from('.sc-intro .point-area img',{
+        scrollTrigger:{
+            trigger:'.point-area',
+            start:'top bottom',
+            end:'bottom top',
+            markers:true,
+            scrub:1
+        },
+        scale:1.4
     })
 
-    $('[data-hover]').on('mouseover',function(){
-        $('.cursor').addClass('cursor-over')
-        $(this).css('cursor','none');
+    // img trigger
+    $('[data-fade]').each(function(i,el){
+        gsap.from(el,{
+            scrollTrigger:{
+                trigger:el,
+                start:"top 80%",
+                end:"bottom top",
+                scrub:1
+            },
+            yPercent:40,
+            opacity:0,
+            duration:0.8
+        })
+    });
+
+    // number trigger
+    gsap.set('.sc-intro .num-list',{y:0})
+    gsap.set('.sc-goal .num-list',{y:0})
+
+    gsap.to('.sc-intro .num-list',{
+        scrollTrigger:{
+            trigger:'.sc-intro .num-list',
+            start:"top 50%",
+            end:"bottom top",
+        },
+        duration:1,
+        y:'-280px'
     })
-    $('[data-hover]').on('mouseleave',function(){
-        $('.cursor').removeClass('cursor-over')
+    gsap.to('.sc-goal .num-list',{
+        scrollTrigger:{
+            trigger:'.sc-goal .num-list',
+            start:"top 50%",
+            end:"bottom top",
+        },
+        duration:1,
+        y:'-560px'
     })
-    
-    // 바탕색 변하는거
-    // 스크롤 트리거 이벤트
+
+
+
+    // background color change
     // 퍼스펙티브 3d
-    // 사진 스크롤 트리거
     // 원 호버하면 따라다니는거
     // 로딩 
     // 호버시 사진 펼쳐지기
