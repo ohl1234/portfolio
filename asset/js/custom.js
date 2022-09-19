fab7bbb5458ac70eb6b205d478a840aaeba3fe26
ohhyerim <tmvkfmxk00@naver.com>
2022-09-19 20:45:17 +0900

    js 두번째

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,7 +1,14 @@
 $(function(){
-    //moust event
-    $(window).resize(function(){ 
-        if (window.innerWidth > 767) {  
+    ScrollTrigger.matchMedia({
+        // large
+
+
+        "(min-width: 1024px)": function() {
+
+            /**
+             * moust event 
+             */
+
             $(window).mousemove(function(e){
                 e.preventDefault();
                 gsap.to('.cursor',{
@@ -18,73 +25,141 @@ $(function(){
             $('[data-hover]').on('mouseleave',function(){
                 $('.cursor').removeClass('cursor-over')
             })
+
+            /**
+             * number trigger
+             */
+            gsap.set('.sc-intro .num-list',{y:0})
+            gsap.set('.sc-goal .num-list',{y:0})
+
+            gsap.to('.sc-intro .num-list',{
+                scrollTrigger:{
+                    trigger:'.sc-intro .num-list',
+                    start:"top 50%",
+                    end:"bottom top",
+                },
+                duration:1,
+                y:'-280px'
+            })
+            gsap.to('.sc-goal .num-list',{
+                scrollTrigger:{
+                    trigger:'.sc-goal .num-list',
+                    start:"top 50%",
+                    end:"bottom top",
+                },
+                duration:1,
+                y:'-560px'
+            })
+        },
+        // medium
+        "(min-width: 768px) and (max-width: 1024px)": function() { 
+
+            /**
+             * number trigger
+             */
+            gsap.set('.sc-intro .num-list',{y:0})
+            gsap.set('.sc-goal .num-list',{y:0})
+
+            gsap.to('.sc-intro .num-list',{
+                scrollTrigger:{
+                    trigger:'.sc-intro .num-list',
+                    start:"top 50%",
+                    end:"bottom top",
+                },
+                duration:1,
+                y:'-200px'
+            })
+            gsap.to('.sc-goal .num-list',{
+                scrollTrigger:{
+                    trigger:'.sc-goal .num-list',
+                    start:"top 50%",
+                    end:"bottom top",
+                },
+                duration:1,
+                y:'-400px'
+            })
+        },
+        // small
+        "(max-width: 767px)": function() {
+
+            /**
+             * number trigger
+             */
+            gsap.set('.sc-intro .num-list',{y:0})
+            gsap.set('.sc-goal .num-list',{y:0})
+
+            gsap.to('.sc-intro .num-list',{
+                scrollTrigger:{
+                    trigger:'.sc-intro .num-list',
+                    start:"top 80%",
+                    end:"bottom top",
+                },
+                duration:1,
+                y:'-200px'
+            })
+            gsap.to('.sc-goal .num-list',{
+                scrollTrigger:{
+                    trigger:'.sc-goal .num-list',
+                    start:"top 80%",
+                    end:"bottom top",
+                },
+                duration:1,
+                y:'-400px'
+            })
+        },
+        // all
+        "all": function() {
+
+            /**
+             * Trigger event
+             */
+            gsap.set('.sc-visual .txt-line1',{xPercent:-180})
+            gsap.set('.sc-visual .txt-line2',{xPercent:180})
+            const visualAni = gsap.timeline({
+                    scrollTrigger:{
+                    trigger:'.sc-visual .sc-title',
+                    start:'top bottom',
+                    end:'bottom top',
+                    scrub:1
+                }
+            });
+            visualAni.addLabel('a')
+            .to('.sc-visual .txt-line1',{xPercent:100},'a')
+            .to('.sc-visual .txt-line2',{xPercent:-100},'a')
+
+            /**
+             * Trigger 
+             */
+            $('[data-fade]').each(function(i,el){
+                gsap.from(el,{
+                    scrollTrigger:{
+                        trigger:el,
+                        start:"top 120%",
+                        end:"bottom top",
+                        scrub:1,
+                        markers:true
+                    },
+                    yPercent:15,
+                    opacity:0,
+                    duration:0.3
+                })
+             });
+
+            /**
+             * img trigger
+             */
+            gsap.from('.sc-intro .point-area img',{
+                scrollTrigger:{
+                    trigger:'.point-area',
+                    start:'top bottom',
+                    end:'bottom top',
+                    markers:true,
+                    scrub:1
+                },
+                scale:1.4
+            })
         }
-        
-    }).resize(); 
-
-    // Trigger event
-    gsap.set('.sc-visual .txt-line1',{xPercent:-180})
-    gsap.set('.sc-visual .txt-line2',{xPercent:180})
-    const visualAni = gsap.timeline({
-            scrollTrigger:{
-            trigger:'.sc-visual .sc-title',
-            start:'top bottom',
-            end:'bottom top',
-            scrub:1
-        }
-    });
-    visualAni.addLabel('a')
-    .to('.sc-visual .txt-line1',{xPercent:100},'a')
-    .to('.sc-visual .txt-line2',{xPercent:-100},'a')
-
-    gsap.from('.sc-intro .point-area img',{
-        scrollTrigger:{
-            trigger:'.point-area',
-            start:'top bottom',
-            end:'bottom top',
-            markers:true,
-            scrub:1
-        },
-        scale:1.4
-    })
-
-    // img trigger
-    $('[data-fade]').each(function(i,el){
-        gsap.from(el,{
-            scrollTrigger:{
-                trigger:el,
-                start:"top 80%",
-                end:"bottom top",
-                scrub:1
-            },
-            yPercent:40,
-            opacity:0,
-            duration:0.8
-        })
-    });
-
-    // number trigger
-    gsap.set('.sc-intro .num-list',{y:0})
-    gsap.set('.sc-goal .num-list',{y:0})
-
-    gsap.to('.sc-intro .num-list',{
-        scrollTrigger:{
-            trigger:'.sc-intro .num-list',
-            start:"top 50%",
-            end:"bottom top",
-        },
-        duration:1,
-        y:'-280px'
-    })
-    gsap.to('.sc-goal .num-list',{
-        scrollTrigger:{
-            trigger:'.sc-goal .num-list',
-            start:"top 50%",
-            end:"bottom top",
-        },
-        duration:1,
-        y:'-560px'
-    })
+      });
 
 
 
